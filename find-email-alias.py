52f9bda2c6deb4c3c0b3cfb1b72e45d363759881
Philip A Larson <dynendal@gmail.com>
2019-10-01 16:17:36 -0700

    Update find-email-alias.py

--- find-email-alias.py
+++ find-email-alias.py
@@ -4,8 +4,10 @@ import difflib
 possibilities = ['ab2-flux', 'ab2-texture', 'ab2-fx']
 wanted = 'Team Flux'
 
-wanted = tuple(wanted.lower().split(' '))
+wanted = wanted.lower().split(' ')
 
 matcher = lambda x: difflib.get_close_matches(x, possibilities)
 
-print([matcher(match) for match in wanted if matcher(match)])
+[result] = [matcher(match) for match in wanted if matcher(match)][0]
+
+print(result)
